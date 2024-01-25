eb741b916e4d2677ebbf24bf020c7cf3817c5240
Heckrath, Andre <andre.heckrath@pstec.eu>
2024-01-25 12:39:53 +0100

    Lab Dateien hinzugefügt

---
+++ BlazorLab6/Data/ToastItem.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorLab6.Data
+{
+    public class ToastItem
+    {
+        public string Titel { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ BlazorLab6/Data/ToasterVM.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorLab6.Data
+{
+    public class ToasterVM
+    {
+        public List<ToastItem> Toasts { get; set; }
+
+        public event Action OnToastsUpdated;
+
+        public ToasterVM()
+        {
+            Toasts = new List<ToastItem>();
+        }
+
+        public void Add(ToastItem _item)
+        {
+            Toasts.Add(_item);
+            OnToastsUpdated?.Invoke();
+        }
+
+        public void Remove(ToastItem _item)
+        {
+            Toasts.Remove(_item);
+            OnToastsUpdated?.Invoke();
+        }
+    }
+}
